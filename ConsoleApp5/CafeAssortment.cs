57b833edec57ecff353ac35be3e4bdeef2954d92
Dasadno <Yakaleks96@gmail.com>
2025-03-16 18:59:39 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp5/CafeAssortment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class CafeAssortment
+    {
+        private string _dishName;
+        private double _price;
+        private string _category;
+        private double _weight;
+
+        public virtual string Info() => $"Название блюда: {_dishName}, цена: {_price}, категория: {_category}";
+
+        public virtual string GetDishPrice()
+        {
+            double res = _price / _weight;
+            return $"Стоимость одного грамма блюда будет равна: {res}";
+        }
+
+        public virtual string CostOfDrink(int percent)
+        {
+            double res;
+            res = _price - (_price * percent / 100);
+            return $"Стоимость напитка со скидкой {percent}, будет: {res}";
+        }
+
+
+        public CafeAssortment(string dishName, double price, string category, double weight)
+        {
+            _dishName = dishName;
+            _price = price;
+            _category = category;
+            _weight = weight;
+        }
+    }
+}
---
+++ ConsoleApp5/Parrot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Parrot : Animals
+    {
+        public override string Info() => $"{base.Info()}";
+
+
+        public Parrot(string name, string breed, string birthDate, double weight)
+            : base(name, breed, birthDate, weight)
+        {
+
+        }
+    }
+}
